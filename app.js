2eb72ae45f9349e9b594eebd64aa0f1619774abc
Vishnu Satheesh <vishnusatheeshpulickal555@gmail.com>
2021-03-24 23:36:46 +0530

    simple login page, bootstrap links, login routes are created

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const middleware = require('./middleware');
 
 app.set("view engine","pug");
 app.set("views","views")
 
-app.get('/',(req,res)=>{
-    res.status(200).render('home');
+//Routes
+const loginRoute = require('./routes/loginRoutes')
+app.use('/login',loginRoute)
+
+app.get('/',middleware.requireLogin,(req,res,next) => {
+
+    var payload = {
+        pageTitle : "Home"
+    }
+    res.status(200).render('home',payload);
 })
 
 const port = process.env.PORT || 3000;
