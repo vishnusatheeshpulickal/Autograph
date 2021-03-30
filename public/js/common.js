0338076f6c4181346e8450e0728100e78772efb6
Vishnu Satheesh <vishnusatheeshpulickal555@gmail.com>
2021-03-31 01:00:56 +0530

    Created the posts mongoose schema and inserting the posts into the database

--- public/js/common.js
+++ public/js/common.js
@@ -13,3 +13,15 @@ $("#postTextarea").keyup((event)=>{
 
     submitButton.prop("disabled", false);
 })
+
+$("#submitPostButton").click(()=>{
+    var button = $(event.target);
+    var textbox = $("#postTextarea");
+    var data = {
+        content : textbox.val()
+    }
+
+    $.post('/api/posts',data,(postData,status,xhr)=>{
+       console.log(postData)
+    })
+})
---
+++ routes/api/posts.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const Post = require('../../schemas/PostSchema');
+const User = require('../../schemas/UserSchema');
+
+router.get('/',(req,res,next) => {
+ 
+})
+
+router.post('/',async(req,res,next) => {
+  
+ if(!req.body.content){
+     console.log("Content param is not sent with request");
+     return res.sendStatus(400);
+    }
+   var postData = {
+      content:req.body.content,
+      postedBy:req.session.user
+   }
+
+    Post.create(postData)
+     .then(async newPost => {
+         newPost = await User.populate(newPost,{path : "postedBy"})
+         res.status(200).send(newPost);
+     })
+     .catch(error=>{
+         console.log(error);
+         res.sendStatus(400);
+     })
+})
+
+module.exports = router;
