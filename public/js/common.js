affe5903db7ed079a54d503c869491cfe3a95dde
Vishnu Satheesh <vishnusatheeshpulickal555@gmail.com>
2021-03-31 15:22:30 +0530

    comment,retweet,like buttons are added, Verified sign are added, Output of the commet is now coming from backend, user details are also visible in the post container

--- public/js/common.js
+++ public/js/common.js
@@ -22,6 +22,56 @@ $("#submitPostButton").click(()=>{
     }
 
     $.post('/api/posts',data,(postData,status,xhr)=>{
-       console.log(postData)
+       var html = createPostHtml(postData);
+       $(".postsContainer").prepend(html)
+       textbox.val("");
+       button.prop("disabled",true)
     })
 })
+
+function createPostHtml(postData){
+    const postedBy = postData.postedBy;
+    const displayName = postedBy.firstName + " " + postedBy.lastName;
+    const timeStamp ="later";
+    var verified = "";
+
+    if(postedBy.isVerified){
+     verified = '/images/verified.svg'
+    }
+
+    return `<div class='post'>
+                <div class='mainContentContainer'>
+                    <div class='userImageContainer'>
+                        <image src='${postedBy.profilePic}'>
+                    </div>
+                    <div class='postContentContainer'>
+                       <div class='header'>
+                         <a href='/profile/${postedBy.username}' class='displayName'>${displayName}</a>
+                         <img class='verified' src='${verified}'>
+                         <span class='username'>@${postedBy.username}</span>
+                         <span class='date'>${timeStamp}</span>
+                       </div>
+                       <div class='postBody'>
+                         <span>${postData.content}</span>
+                       </div>
+                       <div class='postFooter'>
+                          <div class='postButtonContainer'>
+                             <button>
+                                 <i class='far fa-comment'></i>
+                             </button>
+                          </div>
+                          <div class='postButtonContainer'>
+                             <button>
+                                 <i class='fas fa-retweet'></i>
+                             </button>
+                          </div>
+                          <div class='postButtonContainer'>
+                             <button>
+                                 <i class='far fa-heart'></i>
+                             </button>
+                          </div>
+                       </div>
+                    </div>
+                </div>
+             </div>`
+}
